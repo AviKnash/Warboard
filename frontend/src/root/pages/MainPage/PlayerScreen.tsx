7fe7e5d5a1e2289fcdd671357a4df58f68e28499
AviKnash <bakaanash@yahoo.com>
2025-02-23 22:31:57 +0530

    Updated several items

--- frontend/src/root/pages/MainPage/PlayerScreen.tsx
+++ frontend/src/root/pages/MainPage/PlayerScreen.tsx
@@ -2,45 +2,54 @@ import { GameProps } from "@/types";
 import TypingParagraph from "../../components/Game/TypingParagraph";
 import { useState } from "react";
 import WorkerBot from "./WorkerBot";
-import WarbyAttackSide from '/WarbyAttackSide.gif'
-import IcyFloor from '/IcyFloorFlat.png'
 import { truncate } from "@/lib/utils";
-
+import { SparklesCore } from "@/components/ui/sparkles";
 
 const PlayerScreen = ({
   name,
   ioInstance,
   gameStatus,
   paragraph,
-  typingErrors
+  typingErrors,
 }: GameProps) => {
-
-const [mistake,setMistake] = useState<boolean>(false)
+  const [mistake, setMistake] = useState<boolean>(false);
 
   return (
-    <div
-      className="flex-1 rounded-lg m-4 shadow-md flex items-center justify-center"
-    >
+    <div className="flex-1 rounded-lg m-4 shadow-md flex items-center justify-center">
       {gameStatus === "not-started" ? (
         <div className="relative w-full">
-        <div className="flex-grow flex-row flex align-middle">
-          <div className="flex items-center flex-1">
-
-          <h1 className="md:text-6xl sm:text-3xl pr-10">{truncate(name as string)}</h1>
+          <div className="flex-grow flex-row flex align-middle">
+            <div className="flex items-center flex-1">
+              <h1 className="md:text-6xl sm:text-3xl pr-10">
+                {truncate(name as string)}
+              </h1>
+            </div>
           </div>
-          <img draggable="false" className="w-[10rem]" src={WarbyAttackSide}/>
-                
-        </div>
-        <div className="absolute h-full w-full flex bottom-[-1rem] z-0">
-    <img draggable="false" className="w-full" src={IcyFloor} />
-  </div>
+          <div className="absolute h-full w-full flex bottom-[-1rem] z-0">
+            <SparklesCore
+              id="playersparkles"
+              background="transparent"
+              minSize={0.6}
+              maxSize={1.4}
+              particleDensity={200}
+              className="w-full h-full"
+              particleColor="#FFFFFF"
+            />
+          </div>
         </div>
       ) : (
         <>
-          <div className= "flex-grow h-full">
-          {mistake && <div className="fixed top-0 left-0 w-full h-full bg-red-700 bg-opacity-20 animate-flash pointer-events-none"/>}
-            <TypingParagraph setMistake={setMistake} typingErrors={typingErrors} ioInstance={ioInstance} paragraph={paragraph} />
-          <WorkerBot />
+          <div className="flex-grow h-full">
+            {mistake && (
+              <div className="fixed top-0 left-0 w-full h-full bg-red-700 bg-opacity-20 animate-flash pointer-events-none" />
+            )}
+            <TypingParagraph
+              setMistake={setMistake}
+              typingErrors={typingErrors}
+              ioInstance={ioInstance}
+              paragraph={paragraph}
+            />
+            <WorkerBot />
           </div>
         </>
       )}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 module.exports = {
   darkMode: ["class"],
   content: [
@@ -82,5 +86,17 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"),addVariablesForColors],
+};
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
 }
